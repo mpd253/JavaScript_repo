ab3140d9687443040cd45b27df511958af06f7d3
mpd253 <mpd253@naver.com>
2023-05-26 01:06:19 +0900

    study

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,9 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add() {
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, '');
+}
+
+addBtn.addEventListener('click', add);
