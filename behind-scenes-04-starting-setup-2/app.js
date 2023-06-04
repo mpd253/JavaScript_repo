843e4988d4dddd632f02781499d6026728a2bcff
mpd253 <mpd253@naver.com>
2023-06-05 05:42:45 +0900

    ES6, var, let, const, block scope, global scope, primitive&reference value, garbage collection

--- behind-scenes-04-starting-setup-2/app.js
+++ behind-scenes-04-starting-setup-2/app.js
@@ -2,10 +2,23 @@ const addListenerBtn = document.getElementById('add-listener-btn');
 const clickableBtn = document.getElementById('clickable-btn');
 const messageInput = document.getElementById('click-message-input');
 
+// Garbage Collector
+let person = { name: 'Max' };
+
+person = null;
+
 function printMessage() {
   const value = messageInput.value;
   console.log(value || 'Clicked me!');
 }
+
+// 위의 것과 같음
+// function addListener() {
+//   clickableBtn.addEventListener('click', function() {
+//     const value = messageInput.value;
+//     console.log(value || 'Clicked me!');
+//   });
+// }
 
 function addListener() {
   clickableBtn.addEventListener('click', printMessage);
