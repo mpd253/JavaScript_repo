95301a79abd9f2c8d3b3d0b5ce99bd20a90610fd
mpd253 <mpd253@naver.com>
2023-06-05 02:04:58 +0900

    Section 5 start

---
+++ var vs let & const/app.js
@@ -0,0 +1,27 @@
+// var name = 'Max';
+// let hobbies;
+
+// if (name === "Max") {
+//   hobbies = ['Sports', 'Cooking'];
+//   console.log(hobbies);
+// }
+
+// function greet() {
+//   var age = 30;
+//   var name = "Manuel";
+//   console.log(name, age, hobbies);
+// }
+
+// console.log(name, hobbies);
+
+// greet();
+
+console.log(userName);
+
+// error
+// let userName = 'Max';
+
+// undefined
+var userName = 'Max';
+// 다시 선언해도 새로 바뀜, Manuel
+var userName = 'Manuel';
