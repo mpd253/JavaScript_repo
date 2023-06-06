0b47f3842f7ac7c816de7747af0ff07b35371b0d
mpd253 <mpd253@naver.com>
2023-06-07 00:38:37 +0900

    Section 6 ~ing

---
+++ functions-01-starting-code/app.js
@@ -0,0 +1,139 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
+
+let gameIsRunning = false;
+
+const getPlayerChoice = () => {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ''
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
+
+const getComputerChoice = () => {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+// const start = function() {
+//   console.log('Game is starting...');
+// };
+
+// // property에 함수를 저장할 때, 키나 객체에 저장한 함수 = 메서드
+// const person = {
+//   name: 'Max',
+//   greet: function greet() {
+//     console.log('Hello, there!');
+//   }
+// }
+
+// person.greet();
+
+// // 함수 그 자체로도 하나의 객체가 된다.
+// console.dir(startGame);
+
+// anonymous function
+// 함수 뒤에 ()를 붙이면 한번만 실행한다.
+// 익명 함수에 이름을 붙이면 오류 잡기 편하다.
+
+// // Arrow Function
+// const add = (a, b) => a + b;
+
+// const add2 = function(a, b) {
+//   return a+b;
+// }
+
+const getWinner = (cChoice, pChoice = DEFAULT_USER_CHOICE) =>
+  cChoice === pChoice
+    ? RESULT_DRAW
+    : (cChoice === ROCK && pChoice === PAPER) ||
+      (cChoice === PAPER && pChoice === SCISSORS) ||
+      (cChoice === SCISSORS && pChoice === ROCK)
+    ? RESULT_PLAYER_WINS
+    : RESULT_COMPUTER_WINS;
+
+  // if (cChoice === pChoice) {
+  //   return RESULT_DRAW;
+  // } else if (
+  //   (cChoice === ROCK && pChoice === PAPER) ||
+  //   (cChoice === PAPER && pChoice === SCISSORS) ||
+  //   (cChoice === SCISSORS && pChoice === ROCK)
+  // ) {
+  //   return RESULT_PLAYER_WINS;
+  // } else {
+  //   return RESULT_COMPUTER_WINS;
+  // }
+
+startGameBtn.addEventListener('click', () => {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log('Game is starting...');
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  let winner;
+  if (playerChoice) {
+    winner = getWinner(computerChoice, playerChoice);
+  } else {
+    winner = getWinner(computerChoice);
+  }
+  let message = `You picked ${playerChoice || DEFAULT_USER_CHOICE}, computer picked ${computerChoice}, therefore you `
+  if (winner === RESULT_DRAW) {
+    message = message + 'had a draw.';   
+  } else if (winner === RESULT_PLAYER_WINS) {
+    message = message + 'won.';
+  } else {
+    message = message + 'lost.';
+  }
+  alert(message);
+  gameIsRunning = false;
+});
+
+// not related to game
+
+const sumUp = (...numbers) => {
+  let sum = 0;
+  for (const num of numbers) {
+    sum += num;
+  }
+  return sum;
+};
+
+const subtractUp = function(...numbers) {
+  let sum = 0;
+  for (const num of numbers) {
+    sum -= num;
+  }
+  return sum;
+}
+
+// const subtractUp = function() {
+//   let sum = 0;
+//   for (const num of arguments) {    // don't use this.
+//     sum -= num;
+//   }
+//   return sum;
+// }
+
+console.log(sumUp(1, 5, 10, -3, 6, 10));
+console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
+console.log(subtractUp(1, 10, 15, 20));
