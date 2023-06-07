72298076b1d4918a180a790901fbc60ec33fb3ad
ChoiYoungKyu <mpd253@naver.com>
2023-06-07 20:46:35 +0900

    기본 인자, rest 매개변수&인자, bind() 함수, 콜백 함수 etc..

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -29,55 +29,76 @@ function writeToLog(
   console.log(logEntries);
 }
 
-function calculateResult(calculationType) {
-  const enteredNmber = getUserInput();
-  if (
-    calculationType === 'ADD' &&
-    calculationType === 'SUBTRACT' &&
-    calculationType === 'MULTIPLY' &&
-    calculationType === 'DIVIDE' ||
-    // not Falsy = !0
-    !enteredNumber
-  ) {
-    return; 
-  }
+// function calculateResult(calculationType) {
+//   const enteredNmber = getUserInput();
+//   if (
+//     calculationType === 'ADD' &&
+//     calculationType === 'SUBTRACT' &&
+//     calculationType === 'MULTIPLY' &&
+//     calculationType === 'DIVIDE' ||
+//     // not Falsy = !0
+//     !enteredNumber
+//   ) {
+//     return; 
+//   }
 
+//   const initialResult = currentResult;
+//   let mathOperator;
+//   if (calculationType === 'ADD') {
+//     currentResult += enteredNumber;
+//     mathOperator = '+';
+//   } else if (calculationType === 'SUBTRACT') {
+//     currentResult -= enteredNumber;
+//     mathOperator = '-';
+//   } else if (calculationType === 'MULTIPLY') {
+//     currentResult *= enteredNumber;
+//     mathOperator = '*';
+//   } else {
+//     currentResult /= enteredNumber;
+//     mathOperator = '/';
+//   }
+//   createAndWriteOutput(mathOperator, initialResult, enteredNumber)
+//   writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+// }
+
+// function add() {
+//   calculationType('ADD');
+// }
+
+// function subtract() {
+//   calculationType('SUBTRACT');
+// }
+
+// function multiply() {
+//   calculationType('MULTIPLY');
+// }
+
+// function divide() {
+//   calculationType('DIVIDE');
+// }
+
+function calculate(operation) {
+  const enteredNumber = getUserInput();
   const initialResult = currentResult;
-  let mathOperator;
-  if (calculationType === 'ADD') {
+  let operator;
+  if (operation === 'ADD') {
     currentResult += enteredNumber;
-    mathOperator = '+';
-  } else if (calculationType === 'SUBTRACT') {
+    operator = '+';
+  } else if (operation === 'SUBTRACT') {
     currentResult -= enteredNumber;
-    mathOperator = '-';
-  } else if (calculationType === 'MULTIPLY') {
+    operator = '-';
+  } else if (operation === 'MULTIPLY') {
     currentResult *= enteredNumber;
-    mathOperator = '*';
+    operator = '*';
   } else {
     currentResult /= enteredNumber;
-    mathOperator = '/';
+    operator = '/';
   }
-  createAndWriteOutput(mathOperator, initialResult, enteredNumber)
-  writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+  createAndWriteOutput(operator, initialResult, enteredNumber);
+  writeToLog(operation, initialResult, enteredNumber, currentResult);
 }
 
-function add() {
-  calculationType('ADD');
-}
-
-function subtract() {
-  calculationType('SUBTRACT');
-}
-
-function multiply() {
-  calculationType('MULTIPLY');
-}
-
-function divide() {
-  calculationType('DIVIDE');
-}
-
-addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', subtract);
-multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click', divide);
+addBtn.addEventListener('click', calculate.bind(this, 'ADD'));
+subtractBtn.addEventListener('click', calculate.bind(this, 'SUBTRACT'));
+multiplyBtn.addEventListener('click', calculate.bind(this, 'MULTIPLY'));
+divideBtn.addEventListener('click', calculate.bind(this, 'DIVIDE'));
